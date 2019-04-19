5209c7dd8fbfe32ba5bb382c3ebdb19f7e3babc1
fliaping <xuping@dianwoda.com>
2019-04-19 16:07:15 +0800

    优化deploy

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ datetime=`date +"%Y%m%d%H%M%S"` && \
 filename=open-portal-docs_v$datetime.zip && \
 zip -r $filename * && \
 ossutil64 cp ./$filename oss://dwd-open-bucket/open-portal-static/website_zip/ && \
-echo "\n\nhttps://dwd-open-bucket.oss-cn-hangzhou.aliyuncs.com/open-portal-static/website_zip/$filename\n"
+echo -e "\n\nhttps://dwd-open-bucket.oss-cn-hangzhou.aliyuncs.com/open-portal-static/website_zip/$filename\n"
