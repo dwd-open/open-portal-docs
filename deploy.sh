e9be842793a5e91615fc5fa3e32e3a98272fb90d
fliaping <xuping@dianwoda.com>
2019-04-19 16:03:19 +0800

    添加签名代码示例

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #/bin/bash
-git pull
-rm -rf _book
-gitbook build
-cd _book
-rm deploy.sh
-datetime=`date +"%Y%m%d%H%M%S"`
-filename=open-portal-docs_v$datetime.zip
-zip -r $filename *
-ossutil64 cp ./$filename oss://dwd-open-bucket/open-portal-static/website_zip/
+git pull && \
+rm -rf _book && \
+gitbook build && \
+cd _book && \
+rm deploy.sh && \
+datetime=`date +"%Y%m%d%H%M%S"` && \
+filename=open-portal-docs_v$datetime.zip && \
+zip -r $filename * && \
+ossutil64 cp ./$filename oss://dwd-open-bucket/open-portal-static/website_zip/ && \
 echo "\n\nhttps://dwd-open-bucket.oss-cn-hangzhou.aliyuncs.com/open-portal-static/website_zip/$filename\n"
